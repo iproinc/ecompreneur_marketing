e5352181053865ebb0072bd1f8b39cd38fbf4e5f
simonmengyu <simonh@iprogroup.ca>
2019-01-08 18:08:42 -0800

    new deploy file

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,8 @@
 
 echo Start to deploy on first server
 
-ssh deploy@ec2-18-216-230-158.us-east-2.compute.amazonaws.com 'cd apps/ecompreneur/shared/public/ecompreneur_marketing; git pull'
+ssh deploy@ec2-18-216-230-158.us-east-2.compute.amazonaws.com 'cd apps/ecompreneur/shared/public; git pull'
 
 echo Start to deploy on second server
 
-ssh deploy@ec2-18-188-4-39.us-east-2.compute.amazonaws.com 'cd apps/ecompreneur/shared/public/ecompreneur_marketing; git pull'
+ssh deploy@ec2-18-188-4-39.us-east-2.compute.amazonaws.com 'cd apps/ecompreneur/shared/public; git pull'
