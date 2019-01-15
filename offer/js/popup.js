d34e1dc1b246199310cce37a93477ebbf7b30ed8
simonmengyu <simonh@iprogroup.ca>
2019-01-15 11:39:04 -0800

    ajax call

--- offer/js/popup.js
+++ offer/js/popup.js
@@ -1,67 +1,40 @@
-
-async function check_account() {
-  let product = await get_dropified_bundle(email);
-
+function check_account(email){
+  check_account_ajax(email, ( response ) => {
+    if (response.msg == "error"){
+      console.log("error -    - ");
+    }  else if (response.msg == "ok") {
+      console.log(tiny+ "tiny");
+      console.log(shop+ "shop");
+    }
+  });
 }
-
 
 // go to ep app to check account
-async function get_account(dropified_products_id) {
-  return new Promise (
-    (resolve, reject) => {
-      $.ajax({
-        url: "https://app.dropified.com/product/mapping/bundle/"+dropified_products_id,
-        type: "GET"
-      }).then ( (response) => { //success ajax
-        let variants = [];
-        let pattern = new RegExp("var shopify_product =([^\n]+);\s*\n");
-        let match = pattern.exec(response);
-        // get shopify_product from regex
-        eval(match[0]);
+function check_account_ajax(email, callback){
+  $.ajax({
+    url: 'https://3775be74.ngrok.io/check_recurly_account',
+    type: "POST",
+    data: {
+      email: email
+    },
+    dataType: "json",
+    success: ( response, status ) => {
+      console.log(status);
+      console.log(response);
+      if (callback){
+        callback(response);
+      }
+    },
+    error: ( response, status ) => {
+      console.log(status);
+      console.log(response);
+    }
+  });
+};
 
-        for (let variant_index in shopify_product.variants){
-            let bundle_item = shopify_product.variants[variant_index].products;
-            let bundles = [];
-            // include itself as first bundle
-            let self_variant_id = shopify_product.variants[variant_index].id;
-            let self_data = DROPIFIEDMAP.get(self_variant_id.toString());
-            let bundle_self_item = {
-              data: self_data,
-              quantity: 1
-            }
-            // put self in bundle if bundle is empty
-            if (bundle_item.length==0) {
-              bundles.push(bundle_self_item);
-            }
-            // pack each bundle
-            for (let bundle_index in bundle_item) {
-                let quantity = bundle_item[bundle_index].quantity;
-                variant_id = bundle_item[bundle_index].variant_id;
-                let data = DROPIFIEDMAP.get(variant_id);
-                if (!data) {
-                  continue;
-                }
-                let bundle_item_data = {
-                    data: data,
-                    quantity: quantity
-                }
-                bundles.push(bundle_item_data);
-            }
-            let variant = {
-                shopify_variant_id: shopify_product.variants[variant_index].id,
-                bundles: bundles
-            }
-            variants.push(variant);
-        }
 
-        product = {
-          shopify_product_id: shopify_product_id,
-          variants: variants
-        }
-        resolve(product);
-      },
-      (response) => { // fail ajax
-        reject(response);
-      })
-  })
-}
+
+// msg: "ok",
+// tiny: tiny,
+// shop: shop
+// }
